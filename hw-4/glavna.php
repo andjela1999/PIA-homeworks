d36b01c46dab8ca483019bb486bf188f36967a22
andjela1999 <62012345+andjela1999@users.noreply.github.com>
2021-01-28 13:00:01 +0100

    Add files via upload
    dodata stranica za prijavu, i započeta glavna stranica

---
+++ hw-4/glavna.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+
+
+<head>
+  <title>glavna stranica</title>
+  <script src="https://kit.fontawesome.com/e68961ce89.js" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <meta name="author" content="Andjela Manojlovic" />
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="keywords" content="pia, web programming, html, css,php, responsive design, bootstrap" />
+  <link rel="stylesheet" type="text/css" href="glavna.css">
+</head>
+
+<body>
+  <header id="top"></header>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+            
+  
+            <div class="boxContainer">
+                <table class="elementsContainer">
+                    <tr>
+                        <td>
+                            <input type="text" placeholder="search" class="search">
+                        </td>
+                        <td>
+                            <a href="#"><i class="fas fa-search"></i>
+                            </a>
+                        </td>
+                    </tr>    
+                </table>
+            </div>
+       
+      
+      <ul>
+        
+        <li><a href="#top">go to top</a></li>
+        <li><a href="login.php">Log out</a></li>
+    
+      </ul>
+  </nav>
+
+  <div id="wrap">
+    <ul id="nav">
+        <li><a href="#">Item One<br><img src="image-75.gif" width="100%" height="100%"></a></li>
+        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
+        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
+        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
+        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
+        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
+        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
+    </ul>
+    <ul id="nav">
+        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
+        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
+        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
+        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
+        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
+        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
+        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
+    </ul>
+   
+    <h2>Page Title</h2>
+    <p>Lorem ipsum dolor sit amet consectetuer morbi ut et ut tincidunt. Et Sed eu pellentesque interdum eleifend quis consectetuer adipiscing egestas non. Euismod Vestibulum pede Vivamus Nulla risus eu sed auctor ligula mollis. Nunc fermentum lorem cursus sed quis est ornare leo est pede. Volutpat ut elit semper volutpat mi Integer non metus risus Nam. Tincidunt sed et id sem ac wisi.</p>
+    <p>Et turpis risus ac pede nulla id penatibus Nam sapien venenatis. Malesuada suscipit Phasellus consequat tellus turpis vitae libero eget malesuada amet. Tempus et laoreet et lacus Donec tellus urna lacinia fermentum pharetra. Duis quis parturient sit cursus turpis tellus id nulla sapien tincidunt. Mauris In vel libero neque Fusce pretium elit Donec.</p>
+</div>
+   
+ 
+
+
+ 
+</body>
+</html>
---
+++ hw-4/login.php
@@ -0,0 +1,57 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://kit.fontawesome.com/e68961ce89.js" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+
+
+
+<form action="probam.php" method="POST" class="form">
+  <div class="container">
+    <h1>LOG IN</h1>
+    <p id="firstText">Please fill in this form to log in.</p>
+    <hr>
+
+    <label for="name_uid"><b>Username or email</b></label>
+    <input type="text" placeholder="Enter Your username or email" name="username" id="username" required>
+
+    
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+
+   
+
+    <hr>
+   
+
+    <button type="submit" class="signinbtn">Log in</button>
+    
+  </div>
+  
+  <div class="container signin">
+    <p>Doesn't have an account? Click here: <a href="imdb.php">Create IMDb account</a>.</p>
+  </div>
+  <footer>
+    <div class="row text-center">
+      <div class="col-md-4 smed">
+        <a href="https://www.facebook.com/imdb"><i class="fab fa-facebook"></i></a>
+        <a href="https://www.instagram.com/imdb/"><i class="fab fa-instagram"></i></a> 
+        <a href="https://www.twitch.tv/IMDb"><i class="fab fa-twitch"></i></a>     
+        <a href="https://twitter.com/imdb"><i class="fab fa-twitter"></i></a>
+        <a href="https://www.youtube.com/imdb"><i class="fab fa-youtube"></i></a>
+      </div>
+      
+    </div>
+  </footer>
+
+</form>
+
+</body>
+</html>
