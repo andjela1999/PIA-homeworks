4a8b60d3cbb7be874752a06bd8ebe4725e35e3bf
andjela1999 <62012345+andjela1999@users.noreply.github.com>
2021-01-28 21:34:45 +0100

    glavna stranica koju vidi admin

---
+++ hw-4/admin.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+
+<head>
+  <title>glavna stranica</title>
+  <script src="https://kit.fontawesome.com/e68961ce89.js" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <meta name="author" content="Andjela Manojlovic" />
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="keywords" content="pia, web programming, html, css,php, responsive design, bootstrap" />
+  <link rel="stylesheet" type="text/css" href="admin.css">
+</head>
+
+<body>
+  <header id="top"></header>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+            
+  
+            <div class="boxContainer">
+                <table class="elementsContainer">
+                    <tr>
+                        <td>
+                            <input type="text" placeholder="search" class="search">
+                        </td>
+                        <td>
+                            <a href="#"><i class="fas fa-search"></i>
+                            </a>
+                        </td>
+                    </tr>    
+                </table>
+            </div>
+       
+      
+      <ul>
+        
+        <li><a href="#top">go to top</a></li>
+        <li><a href="login.php">Log out</a></li>
+        <li><a href="addfilm.php">ADD FILM</a></li>
+    
+      </ul>
+  </nav>
+
+  
+   
+ 
+
+
+ 
+</body>
+</html>
