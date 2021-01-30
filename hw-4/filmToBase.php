234458820af805c8b395e996f569880444ac5dc3
andjela1999 <62012345+andjela1999@users.noreply.github.com>
2021-01-30 03:10:33 +0100

    sitne izmene(u bazu je dodata nova kolona(img))

--- hw-4/filmToBase.php
+++ hw-4/filmToBase.php
@@ -9,11 +9,20 @@
     $casts = mysqli_real_escape_string($conn,$_POST['casts']);
     $DateYear = mysqli_real_escape_string($conn,$_POST['year']);
     $duration = mysqli_real_escape_string($conn,$_POST['duration']);
+    $img = mysqli_real_escape_string($conn,$_POST['image']);
 
-    $sql = "INSERT INTO films (title,descript,genre,director,production,casts,DateYear,duration ) VALUES ('$title','$descript','$genre','$director','$production','$casts','$DateYear','$duration');";
+    $sql = "INSERT INTO films (title,descript,genre,director,production,casts,DateYear,duration,img ) VALUES ('$title','$descript','$genre','$director','$production','$casts','$DateYear','$duration','$img');";
     mysqli_query($conn, $sql);
 
     header("Location: admin.php");
+
+    
+
+
+
+
+
+    
     
 
 
