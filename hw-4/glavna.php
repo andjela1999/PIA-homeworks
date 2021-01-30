2130d67852fc304d107be9ace08ee40b4cd9aecd
andjela1999 <62012345+andjela1999@users.noreply.github.com>
2021-01-30 03:13:24 +0100

    pojavljivanje slika filmova koje dodaje admin

--- hw-4/glavna.php
+++ hw-4/glavna.php
@@ -1,3 +1,7 @@
+<?php
+    include_once 'include.php';
+
+?>
 <!DOCTYPE html>
 <html>
 
@@ -42,30 +46,21 @@
       </ul>
   </nav>
 
-  <div id="wrap">
-    <ul id="nav">
-        <li><a href="#">Item One<br><img src="image-75.gif" width="100%" height="100%"></a></li>
-        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
-        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
-        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
-        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
-        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
-        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
-    </ul>
-    <ul id="nav">
-        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
-        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
-        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
-        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
-        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
-        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
-        <li><a href="#">Item One<br><img src="image-75.gif" width="75" height="75"></a></li>
-    </ul>
-   
-    <h2>Page Title</h2>
-    <p>Lorem ipsum dolor sit amet consectetuer morbi ut et ut tincidunt. Et Sed eu pellentesque interdum eleifend quis consectetuer adipiscing egestas non. Euismod Vestibulum pede Vivamus Nulla risus eu sed auctor ligula mollis. Nunc fermentum lorem cursus sed quis est ornare leo est pede. Volutpat ut elit semper volutpat mi Integer non metus risus Nam. Tincidunt sed et id sem ac wisi.</p>
-    <p>Et turpis risus ac pede nulla id penatibus Nam sapien venenatis. Malesuada suscipit Phasellus consequat tellus turpis vitae libero eget malesuada amet. Tempus et laoreet et lacus Donec tellus urna lacinia fermentum pharetra. Duis quis parturient sit cursus turpis tellus id nulla sapien tincidunt. Mauris In vel libero neque Fusce pretium elit Donec.</p>
-</div>
+  <div id="content">
+      <?php
+          $sql4="SELECT * FROM films";
+          $result4=mysqli_query($conn,$sql4);
+          while($row = mysqli_fetch_array($result4)){
+              echo "<div id='img_div'>";
+                 echo "<p>".$row['title']."</p>";
+                 echo "<img src='fimages/".$row['img']."'>";
+                
+              echo "</div>";
+          }
+      ?>
+  </div>
+
+
    
  
 
